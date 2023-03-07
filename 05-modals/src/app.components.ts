73d3a050f6bf995312cc3858c8bc80431cf24ade
Alexey Filippov <socket.someone@gmail.com>
2023-03-07 09:59:03 +0300

    Update app.components.ts

--- 05-modals/src/app.components.ts
+++ 05-modals/src/app.components.ts
@@ -3,7 +3,7 @@ import { Ctx, Modal, ModalContext } from 'necord';
 
 @Injectable()
 export class AppComponents {
-	@Modal('pizza/:num')
+	@Modal('pizza/:value')
 	public onModal(@Ctx() [interaction]: ModalContext) {
 		return interaction.reply({
 			content: `Your fav pizza : ${interaction.fields.getTextInputValue('pizza')}`
