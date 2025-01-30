237349b53b6e44ad5634efe80129c8504831a3c0
Alexey Filippov <socket.someone@gmail.com>
2025-01-30 21:52:41 +0300

    Update 09-dynamic-guilds/src/app.service.ts

--- 09-dynamic-guilds/src/app.service.ts
+++ 09-dynamic-guilds/src/app.service.ts
@@ -4,6 +4,7 @@ import { ContextOf, Ctx, Once } from 'necord';
 @Injectable()
 export class AppService {
 	private readonly logger = new Logger(AppService.name);
+
 	@Once('ready')
 	public onReady(@Ctx() [client]: ContextOf<'ready'>) {
 		this.logger.log(`Bot is ready! Logged in as ${client.user.tag}`);
